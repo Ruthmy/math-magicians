5233bde0bb197b50c4747637b88986848d76db04
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-08 22:04:56 -0600

    Created 'Quotes.js' component to display a quote from external Ninjas API.

---
+++ src/components/Quotes.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const category = 'happiness';
+
+function Quotes() {
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`,
+          {
+            headers: {
+              'X-Api-Key': 'IJxDQE0w1qFvps1T42CbGw==2TlSWBd5MFRPAT17',
+              'Content-Type': 'application/json',
+            },
+          });
+        const json = await res.json();
+        setData(json[0]);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setData, setIsLoading]);
+
+  if (hasError) return <div>Something went wrong!</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h3>{data.quote}</h3>
+      <p>{data.author}</p>
+    </div>
+  );
+}
+
+export default Quotes;
