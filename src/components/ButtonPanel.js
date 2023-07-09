589dc3b17bd8ad28b1d966b5b0df17736d3b473e
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-08 20:50:59 -0600

    Imported the buttonList from 'bd/ButtonData' -file-

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,29 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Button from './Button';
+import buttonList from '../bd/ButtonsData';
 import '../styles/ButtonPanel.css';
-
-const buttonList = [
-  { label: 'AC', class: 'btn gray' },
-  { label: '+/-', class: 'btn gray' },
-  { label: '%', class: 'btn gray' },
-  { label: '÷', class: 'btn orange' },
-  { label: '7', class: 'btn' },
-  { label: '8', class: 'btn' },
-  { label: '9', class: 'btn' },
-  { label: 'x', class: 'btn orange' },
-  { label: '4', class: 'btn' },
-  { label: '5', class: 'btn' },
-  { label: '6', class: 'btn' },
-  { label: '-', class: 'btn orange' },
-  { label: '1', class: 'btn' },
-  { label: '2', class: 'btn' },
-  { label: '3', class: 'btn' },
-  { label: '+', class: 'btn orange' },
-  { label: '0', class: 'btn zero' },
-  { label: '.', class: 'btn' },
-  { label: '=', class: 'btn orange' },
-];
 
 const ButtonPanel = ({ onHandleClick }) => (
   <div className="ButtonPanel">
