18a96a435668108ee59e99095f1c09a0ceb3b783
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-08 22:06:25 -0600

    Modified 'App.js' - Added in a new div for the 'Quotes' component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Calculator from './components/Calculator';
 import './App.css';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <>
+      <div className="App">
+        <Calculator />
+      </div>
+      <div className="Quotes">
+        <Quotes />
+      </div>
+    </>
   );
 }
 
