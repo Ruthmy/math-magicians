5e538259aa55a55ed62100865f4e4b9946202cd6
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-12 14:09:34 -0600

    Modified 'Quotes.js' -file- Added a loading animation and removed an unnecesary div tag.

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -33,16 +33,24 @@ function Quotes() {
     fetchData();
   }, []);
 
-  if (hasError) return <div>Something went wrong!</div>;
+  if (hasError) return <div><p>Something went wrong!</p></div>;
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <>
+        <div className="lds-ripple">
+          <div />
+          <div />
+        </div>
+        <p>Loading...</p>
+      </>
+    );
+  }
 
   return (
     <div className="Quotes">
-      <div>
-        <h3>{data.quote}</h3>
-        <p>{data.author}</p>
-      </div>
+      <h3>{data.quote}</h3>
+      <p>{data.author}</p>
     </div>
   );
 }
