2eba2effcf8b3d1a46b3af60a6bf0ac51f249571
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-12 14:06:28 -0600

    Added 'Modified 'Menu.js' -file- Added and h1 tag, edited isActive parameter and 'Menu.css' between imports.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import routes from '../bd/MenuData';
+import '../styles/Menu.css';
 
 export default function Menu() {
   return (
     <nav>
+      <h1>Math Magicians</h1>
       <ul>
         {routes.map((route) => (
           <li key={route.text}>
             <NavLink
               style={({ isActive }) => ({
-                color: isActive ? 'red' : 'blue',
+                color: isActive ? '#1b1b1b' : '#6b6b6b',
               })}
               to={route.to}
             >
